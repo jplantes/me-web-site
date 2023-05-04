76b1896ecf74e97c2263936d1e73ec7b9e8bf87b
Juan Pablo Pérez Lantes <jplantes@gmail.com>
2023-05-04 15:52:42 -0300

    StartProject

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="h-screen pt-10">
+    <h1 class="text-5xl text-center">En construcción</h1>
+  </div>
+</template>
---
+++ tailwind.config.cjs
@@ -0,0 +1,4 @@
+/* eslint-disable no-undef */
+module.exports = {
+  content: ['./src/**/*.{vue,js,ts}']
+};
