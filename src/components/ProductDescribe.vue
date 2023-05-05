267e527c3d03c5983a1a1de6fb797dc4c6d9132d
Juan Pablo Pérez Lantes <jplantes@gmail.com>
2023-05-05 12:18:42 -0300

    maqueta página de inicio

---
+++ src/components/ProductDescribe.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="bg-white">
+    <div class="mx-auto grid max-w-2xl grid-cols-1 items-top gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+      <div>
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Mini tabla de entrenamiento</h2>
+        <p class="mt-4 text-gray-500">Liviana y adaptable para que la lleves con vos a todos lados. Te aseguro que va a ser tu aliado no solo para entrar en calor, sino tambien para entrenar cuando no estas en tu muro.</p>
+
+        <dl class="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
+          <div class="border-t border-gray-200 pt-4">
+            <dt class="font-medium text-gray-900">Diseño</dt>
+            <dd class="mt-2 text-sm text-gray-500">Ergonomico para que entrenes tus dedos al maximo con 6 niveles de profundicad</dd>
+          </div>
+          <div class="border-t border-gray-200 pt-4">
+            <dt class="font-medium text-gray-900">Materiales</dt>
+            <dd class="mt-2 text-sm text-gray-500">Fenolico de 21 mm</dd>
+          </div>
+          <div class="border-t border-gray-200 pt-4">
+            <dt class="font-medium text-gray-900">Medidas</dt>
+            <dd class="mt-2 text-sm text-gray-500">250mm x 180mm x 21mm</dd>
+          </div>
+          <div class="border-t border-gray-200 pt-4">
+            <dt class="font-medium text-gray-900">Incluye</dt>
+            <dd class="mt-2 text-sm text-gray-500">Funda y cordin de 3 mm para que la puedas colgar.</dd>
+          </div>
+        </dl>
+      </div>
+      <div class="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
+        <img src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-01.jpg" alt="Walnut card tray with white powder coated steel divider and 3 punchout holes." class="rounded-lg bg-gray-100">
+        <img src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-02.jpg" alt="Top down view of walnut card tray with embedded magnets and card groove." class="rounded-lg bg-gray-100">
+        <img src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-03.jpg" alt="Side of walnut card tray with card groove and recessed card area." class="rounded-lg bg-gray-100">
+        <img src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-04.jpg" alt="Walnut card tray filled with cards and card angled in dedicated groove." class="rounded-lg bg-gray-100">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
