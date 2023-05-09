03de2ca9a4d82dba393db1e8d1c596c058fa6fb6
Juan Pablo Pérez Lantes <jplantes@gmail.com>
2023-05-09 14:22:11 -0300

    Efectos visuales

--- src/views/ArticlesPage.vue
+++ src/views/ArticlesPage.vue
@@ -16,7 +16,7 @@
 
   <div
     v-else
-    class="container mx-auto px-2 bg-white text-gray-600"
+    class="container mx-auto px-2 bg-white text-gray-600 animate__animated animate__fadeIn"
   >
     <h1 class="text-center text-5xl p-12 font-semibold">Articulos dev.to</h1>
 
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative isolate px-6 pt-14 lg:px-8">
+  <div class="relative isolate px-6 pt-14 lg:px-8 animate__animated animate__fadeIn">
     <div class="mx-auto max-w-2xl py-80 sm:py-14 lg:py-56">
       <div class="mb-8 flex justify-center">
         <div class="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
@@ -13,25 +13,24 @@
     </div>
   </div>
 
-  <div class="mx-auto max-w-7xl px-2 lg:px-8">
+  <div class="mx-auto max-w-7xl px-2 lg:p-8">
     <h2 class="text-center text-lg font-semibold leading-8 text-gray-900">Frameworks mas utilizados</h2>
     <div class="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-      <img class="col-span-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1" src="@/assets/images/vue.png" alt="Logo vue" width="158" height="48">
-      <img class="col-span-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1" src="@/assets/images/react.png" alt="Logo react" width="158" height="48">
-      <img class="col-span-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1" src="@/assets/images/angular.png" alt="Logo angular" width="158" height="48">
-      <img class="col-span-2 max-h-16 w-full object-contain sm:col-start-2 lg:col-span-1" src="@/assets/images/node.png" alt="Logo node" width="158" height="48">
-      <img class="col-span-2 max-h-14 w-full object-contain col-start-2 sm:col-start-auto lg:col-span-1" src="@/assets/images/tailwindcss.png" alt="Logo tailwindcss" width="158" height="48">
+      <img data-aos="zoom-in" data-aos-duration="1000" class="col-span-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1" src="@/assets/images/vue.png" alt="Logo vue" width="158" height="48">
+      <img data-aos="zoom-in" data-aos-duration="1000" class="col-span-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1" src="@/assets/images/react.png" alt="Logo react" width="158" height="48">
+      <img data-aos="zoom-in" data-aos-duration="1000" class="col-span-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1" src="@/assets/images/angular.png" alt="Logo angular" width="158" height="48">
+      <img data-aos="zoom-in" data-aos-duration="1000" class="col-span-2 max-h-16 w-full object-contain sm:col-start-2 lg:col-span-1" src="@/assets/images/node.png" alt="Logo node" width="158" height="48">
+      <img data-aos="zoom-in" data-aos-duration="1000" class="col-span-2 max-h-14 w-full object-contain col-start-2 sm:col-start-auto lg:col-span-1" src="@/assets/images/tailwindcss.png" alt="Logo tailwindcss" width="158" height="48">
     </div>
   </div>
 
 
-
-  <div class="mx-auto max-w-7xl px-2 lg:px-8">
+  <div class="mx-auto max-w-7xl px-2 lg:p-8">
     <h2 class="text-center text-lg font-semibold leading-8 text-gray-900">Lenguajes mas utilizados</h2>
     <div class="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-      <img class="col-span-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1" src="@/assets/images/js.png" alt="Logo JavaScript" width="158" height="48">
-      <img class="col-span-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1" src="@/assets/images/css3.png" alt="Logo css3" width="158" height="48">
-      <img class="col-span-2 max-h-24 w-full object-contain col-start-2 sm:col-start-auto lg:col-span-1" src="@/assets/images/html5.png" alt="Logo html5" width="158" height="48">
+      <img data-aos="zoom-in" data-aos-duration="1500" class="col-span-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1" src="@/assets/images/js.png" alt="Logo JavaScript" width="158" height="48">
+      <img data-aos="zoom-in" data-aos-duration="1500" class="col-span-2 max-h-24 w-full object-contain sm:col-start-auto lg:col-span-1" src="@/assets/images/css3.png" alt="Logo css3" width="158" height="48">
+      <img data-aos="zoom-in" data-aos-duration="1500" class="col-span-2 max-h-24 w-full object-contain col-start-2 sm:col-start-auto lg:col-span-1" src="@/assets/images/html5.png" alt="Logo html5" width="158" height="48">
     </div>
   </div>
 
